aef600ed0137b8ac67c86286fac365598ffa2a67
Ian Boll <iangboll@vt.edu>
2019-02-16 18:15:49 -0500

    built context frames and correctly ordered output indices

--- audio_preprocessing.py
+++ audio_preprocessing.py
@@ -2,65 +2,59 @@
 
 import madmom
 import numpy as np
-import matplotlib.pyplot as pyplot
 
 SAMPLE_RATE_HZ = 44100
-FRAME_TIME_MS = 150
-# HOP_TIME_MS = 10 # equivalent to the stride from dance dance convolution
 FRAMES_PER_SECOND = 100
-
-def mono_signal(audio_fpath):
-    signal = madmom.audio.signal.Signal(audio_fpath)
-    return signal
-
-def stereo_signal(audio_fpath):
-    stereo = madmom.audio.signal.Signal(audio_fpath)
-    return stereo
-
-def frame_signal(signal):
-    # hop_size = float(signal.sample_rate) / (float(1000) / float(STRIDE_TIME_MS)) # hop_size can be floating point for madmom
-    frame_size = int(signal.sample_rate / (1000 / FRAME_TIME_MS))
-    fs = madmom.audio.signal.FramedSignal(signal, frame_size=frame_size, fps=FRAMES_PER_SECOND)
-    return fs
-
-def fourier(framed_signal):
-    transformed = madmom.audio.stft.STFT(framed_signal)
-    return transformed
+STEP_SIZE = 1000 / FRAMES_PER_SECOND
+CONTEXT_TIME_MS = 150
 
 def __display_spectrogram(transformed_signal):
     spectrogram = madmom.audio.spectrogram.Spectrogram(transformed_signal)
     pyplot.imshow(spectrogram[:, :FRAMES_PER_SECOND].T, aspect='auto', origin='lower')
-    pyplot.show()
 
 '''
 Builds a functor that accepts a filepath and returns the preprocessed data
 '''
-def mono_signal_processor():
-    frame_size = int(SAMPLE_RATE_HZ / (1000 / FRAME_TIME_MS))
+def mono_signal_processor(frame_time_ms):
+    frame_size = int(SAMPLE_RATE_HZ / (1000 / frame_time_ms))
     signal_reader = madmom.audio.signal.SignalProcessor(num_channels=1, sample_rate=SAMPLE_RATE_HZ)
-    frame_builder = madmom.audio.signal.FramedSignalProcessor(frame_size=frame_size, fps=FRAMES_PER_SECOND)
+    fourier_frame_builder = madmom.audio.signal.FramedSignalProcessor(frame_size=frame_size, fps=FRAMES_PER_SECOND) # framing for the fourier transform
     stft = madmom.audio.stft.ShortTimeFourierTransformProcessor()
     mel_filter = madmom.audio.spectrogram.LogarithmicFilteredSpectrogramProcessor(num_bands=80, log=np.log, fmin=27.5, fmax=16000,
         filterbank=madmom.audio.filters.MelFilterbank)
     # the example I'm working off of also specified a value for LogarithmicFilt...Processor(add=EPSILON)
     # I don't understand the what/why it's there, so I left it out for now
 
-    return madmom.processors.SequentialProcessor([signal_reader, frame_builder, stft, mel_filter])
+    # frame into 150ms chunks
+    context_builder = madmom.audio.signal.FramedSignalProcessor(frame_size=CONTEXT_TIME_MS/STEP_SIZE, hop_size=1)
 
-def old_version():
-    # grab the raw audio file data
-    signal = mono_signal('examples/audio/haraiya-single.ogg')
-    # group it into frames of FRAME_TIME_MS in length, with FRAMES_PER_SECOND as a rate
-    fs = frame_signal(signal)
-    # get a short-time fourier transform of the framed signal
-    stft = fourier(fs)
-    __display_spectrogram(stft)
+    return madmom.processors.SequentialProcessor([signal_reader, fourier_frame_builder, stft, mel_filter, context_builder])
 
-    # stereo = stereo_signal('examples/audio/haraiya.ogg')
-    print(signal)
+'''
+The main entrypoint for this module. Returns a 3D matrix of the form [time x frames x mel-log]
+'''
+def preprocess_file(audio_fpath, *frame_sizes):
+    outputs = []
+    for i in range(len(frame_sizes)):
+        pipeline = mono_signal_processor(frame_sizes[i])
+        output = pipeline(audio_fpath)
+        outputs.append(output)
+    # this pipelining process builds a 3D matrix of the form [frames x time x mel-log]
+    # transpose the first two dimensions so that we have [time x frames x mel-log]
+    return np.transpose(outputs, [1, 0, 2, 3])
 
 if __name__ == '__main__':
-    preprocessor = mono_signal_processor()
-    output = preprocessor('examples/audio/haraiya-single.ogg')
-    __display_spectrogram(output)
-    print(output)
+    import matplotlib.pyplot as pyplot # only use plotting when running as main
+    # frame_sizes = [23, 46, 93]
+    # only use the one frame size since processing takes so long
+    frame_sizes = [46]
+    audio_fpath = 'examples/audio/haraiya.ogg'
+    figure = pyplot.figure(figsize=(len(frame_sizes), 1))
+
+    # this is basically the main entrypoint of the module
+    outputs = preprocess_file(audio_fpath, *frame_sizes)
+    
+    for i in range(len(outputs)):
+        figure.add_subplot(len(frame_sizes), 1, i + 1)
+        __display_spectrogram(outputs[i])
+    pyplot.show()
