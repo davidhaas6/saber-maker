f94cf29d35415e07bcde2f1867ddc523faecf9e8
Ian Boll <iangboll@vt.edu>
2019-02-17 02:55:41 -0500

    made webserver configurable for running in prod

--- src/webserver.py
+++ src/webserver.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
 import flask
+import sys
 
 app = flask.Flask('saber-maker')
+
+# address and port configuration
+port = 8080
+address = '127.0.0.1'
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
+if len(sys.argv) > 2:
+    address = sys.argv[2]
 
 @app.route('/upload')
 def upload():
@@ -17,4 +26,4 @@ def static_index():
     return flask.send_file('www/index.html')
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(host=address, port=port)
